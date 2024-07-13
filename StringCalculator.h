271ce316838b31438052caf2ff59476253ffcbbd
kavyapotnuru258 <kavyapotnuru258@gmail.com>
2024-07-13 17:43:18 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,20 +1,36 @@
 #include "StringCalculator.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+
+void throw_exception_for_negatives(const int* negatives, int count) {
+    if (count > 0) {
+        static char message[1024] = "negatives not allowed: ";
+        char buffer[32];
+        for (int i = 0; i < count; ++i) {
+            sprintf(buffer, "%d", negatives[i]);
+            strcat(message, buffer);
+            if (i < count - 1) {
+                strcat(message, ", ");
+            }
+        }
+        fprintf(stderr, "%s\n", message);
+        exit(EXIT_FAILURE);
+    }
+}
 
 int add(const char* numbers) {
-    if (*numbers == '\0') return 0;
-
-    // Replace newlines with commas
-    char* numbers_copy = strdup(numbers);
-    for (char* p = numbers_copy; *p; p++) {
-        if (*p == '\n') *p = ',';
+    if (numbers[0] == '\0') {
+        return 0;
     }
 
-    char* token = strtok(numbers_copy, ",");
     int sum = 0;
-
-     if (strncmp(numbers_copy, "//", 2) == 0) {
+    int negatives[1024];
+    int negative_count = 0;
+    char* delimiters = ",\n";
+    char* numbers_copy = strdup(numbers);
+    
+    if (numbers_copy[0] == '/' && numbers_copy[1] == '/') {
         char* newline_pos = strchr(numbers_copy, '\n');
         if (newline_pos != NULL) {
             *newline_pos = '\0';
@@ -23,11 +39,18 @@ int add(const char* numbers) {
         }
     }
 
+    char* token = strtok(numbers_copy, delimiters);
     while (token != NULL) {
-        sum += atoi(token);
-        token = strtok(NULL, ",");
+        int num = atoi(token);
+        if (num < 0) {
+            negatives[negative_count++] = num;
+        } else if (num <= 1000) {
+            sum += num;
+        }
+        token = strtok(NULL, delimiters);
     }
 
     free(numbers_copy);
+    throw_exception_for_negatives(negatives, negative_count);
     return sum;
 }
