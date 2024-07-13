6c2b6eaa91bac7558d311583cf3f4d48788e9e0d
kavyapotnuru258 <kavyapotnuru258@gmail.com>
2024-07-13 17:42:49 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -1,80 +1,73 @@
-// #include <gtest/gtest.h>
-// #include "StringCalculator.h"
-
-// TEST(StringCalculatorAddTests, ExpectZeroForEmptyInput) {
-//     int expectedresult = 0;
-//     const char* input = "Hello, world!";
-//     int result = add(input);
-//     ASSERT_EQ(result, expectedresult);
-// }
-
-// TEST(StringCalculatorAddTests, ExpectZeroForSingleZero) {
-//     int expectedresult = 0;
-//     const char* input = "0";
-//     int result = add(input);
-//     ASSERT_EQ(result, expectedresult);
-// }
-
-// TEST(StringCalculatorAddTests, ExpectSumForTwoNumbers) {
-//     int expectedresult = 3;
-//     const char*  input = "1,2";
-//     int result = add(input);
-//     ASSERT_EQ(result, expectedresult);
-// }
-
-// TEST(StringCalculatorAddTests, ExpectSumWithNewlineDelimiter) {
-//     int expectedresult = 6;
-//     const char*  input = "1\n2,3";
-//     int result =add(input);
-//     ASSERT_EQ(result, expectedresult);
-// }
-
-// TEST(StringCalculatorAddTests, IgnoreNumbersGreaterThan1000) {
-//     int expectedresult = 1;
-//     const char*  input = "1,1001";
-//     int result =add(input);
-//     ASSERT_EQ(result, expectedresult);
-// }
-
-// TEST(StringCalculatorAddTests, ExpectSumWithCustomDelimiter) {
-//     int expectedresult = 3;
-//     const char*  input = "//;\n1;2";
-//     int result = add(input);
-//     ASSERT_EQ(result, expectedresult);
-// }
-
 #include <gtest/gtest.h>
 #include "StringCalculator.h"
 
-TEST(StringCalculatorAddTests, ReturnZeroForEmptyString) {
-    ASSERT_EQ(add(""), 0);
+// Test cases for StringCalculator
+
+TEST(StringCalculatorAddTests, ExpectZeroForEmptyInput) {
+    int result = add("");
+    ASSERT_EQ(result, 0);
 }
 
-TEST(StringCalculatorAddTests, ReturnNumberForSingleNumber) {
-    ASSERT_EQ(add("1"), 1);
-    ASSERT_EQ(add("2"), 2);
+TEST(StringCalculatorAddTests, ExpectNumberForSingleNumberInput) {
+    int result = add("1");
+    ASSERT_EQ(result, 1);
 }
 
-TEST(StringCalculatorAddTests, ReturnSumForTwoNumbers) {
-    ASSERT_EQ(add("1,2"), 3);
-    ASSERT_EQ(add("10,20"), 30);
+TEST(StringCalculatorAddTests, ExpectSumForTwoNumbers) {
+    int result = add("1,2");
+    ASSERT_EQ(result, 3);
 }
 
-TEST(StringCalculatorAddTests, ReturnSumForUnknownAmountOfNumbers) {
-    ASSERT_EQ(add("1,2,3"), 6);
-    ASSERT_EQ(add("1,2,3,4,5"), 15);
+TEST(StringCalculatorAddTests, ExpectSumForUnknownAmountOfNumbers) {
+    int result = add("1,2,3,4,5");
+    ASSERT_EQ(result, 15);
 }
 
-TEST(StringCalculatorAddTests, ReturnSumForNewLineDelimiter) {
-    ASSERT_EQ(add("1\n2,3"), 6);
-    ASSERT_EQ(add("1\n2\n3,4"), 10);
+TEST(StringCalculatorAddTests, ExpectSumWithNewlineDelimiter) {
+    int result = add("1\n2,3");
+    ASSERT_EQ(result, 6);
 }
 
-TEST(StringCalculatorAddTests, ReturnSumForCustomDelimiter) {
-    ASSERT_EQ(add("//;\n1;2"), 3);
-    ASSERT_EQ(add("//.\n2.3.4"), 9);
+TEST(StringCalculatorAddTests, ExpectSumWithCustomDelimiter) {
+    int result = add("//;\n1;2");
+    ASSERT_EQ(result, 3);
 }
 
+TEST(StringCalculatorAddTests, ExpectSumWithLongCustomDelimiter) {
+    int result = add("//[***]\n1***2***3");
+    ASSERT_EQ(result, 6);
+}
 
+TEST(StringCalculatorAddTests, ThrowsExceptionForNegativeNumbers) {
+    EXPECT_THROW({
+        add("1,-2,3");
+    }, const char*);
+}
+
+TEST(StringCalculatorAddTests, ThrowsExceptionForMultipleNegativeNumbers) {
+    EXPECT_THROW({
+        add("1,-2,3,-4");
+    }, const char*);
+}
+
+TEST(StringCalculatorAddTests, IgnoreNumbersGreaterThan1000) {
+    int result = add("2,1001");
+    ASSERT_EQ(result, 2);
+}
+
+TEST(StringCalculatorAddTests, ExpectSumWithMultipleCustomDelimiters) {
+    int result = add("//[*][%]\n1*2%3*4");
+    ASSERT_EQ(result, 10);
+}
+
+TEST(StringCalculatorAddTests, ExpectSumWithNewlineAndComma) {
+    int result = add("1,2\n3,4");
+    ASSERT_EQ(result, 10);
+}
+
+TEST(StringCalculatorAddTests, ExpectSumWithCustomDelimiterIncludingBrackets) {
+    int result = add("//[abc]\n1abc2abc3");
+    ASSERT_EQ(result, 6);
+}
 
 
