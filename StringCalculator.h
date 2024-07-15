372d32f13f285e7b5281ca177fd82dc2bb887782
kavyapotnuru258 <kavyapotnuru258@gmail.com>
2024-07-15 17:20:22 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -13,38 +13,29 @@ static int shouldIgnore(int num) {
     return num > 1000;
 }
 
-static char** getDelimiters(char** input) {
-    char** delimiters = (char**)malloc(2 * sizeof(char*));
-    delimiters[0] = strdup(","); // Default delimiter
-    delimiters[1] = NULL;
-
+static char* getDelimiter(char** input) {
     if (strncmp(*input, "//", 2) == 0) {
         char* end = strchr(*input + 2, '\n');
         if (end) {
             *end = '\0';
-            delimiters[0] = strndup(*input + 2, end - (*input + 2));
+            char* delimiter = strndup(*input + 2, end - (*input + 2));
             *input = end + 1; // Skip to numbers
+            return delimiter;
         }
     }
-    return delimiters;
+    return ","; // Default delimiter
 }
 
-static int processToken(char* token) {
-    int num = parseNumber(token);
-    return shouldIgnore(num) ? 0 : num; // Return 0 if ignored
-}
-
-static int sumNumbers(char* str, char** delimiters) {
+static int sumNumbers(char* str, const char* delimiter) {
     int sum = 0;
-    char* token = strtok(str, delimiters[0]);
-    
+    char* token = strtok(str, delimiter);
+
     while (token != NULL) {
-        char* subtoken = strtok(token, "\n");
-        while (subtoken != NULL) {
-            sum += processToken(subtoken);
-            subtoken = strtok(NULL, "\n");
+        int num = parseNumber(token);
+        if (!shouldIgnore(num)) {
+            sum += num;
         }
-        token = strtok(NULL, delimiters[0]);
+        token = strtok(NULL, delimiter);
     }
     return sum;
 }
@@ -56,14 +47,9 @@ static int add(const char* input) {
 
     char* str = strdup(input);
     char* input_copy = strdup(input);
-    char** delimiters = getDelimiters(&input_copy);
+    const char* delimiter = getDelimiter(&input_copy);
     
-    int result = sumNumbers(str, delimiters);
-    
-    free(delimiters[0]);
-    free(delimiters);
-    free(str);
-    free(input_copy);
+    int result = sumNumbers(str, delimiter);
     
     return result;
 }
