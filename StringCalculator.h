e09a20b557451f87240d0720ab6ac5da3f863735
kavyapotnuru258 <kavyapotnuru258@gmail.com>
2024-07-15 16:51:55 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -5,40 +5,53 @@
 #include <stdlib.h>
 #include <string.h>
 
+static int parseNumber(const char* token) {
+    return atoi(token);
+}
+
+static int shouldIgnore(int num) {
+    return num > 1000;
+}
+
+static char* getDelimiter(const char** input) {
+    if (strncmp(*input, "//", 2) == 0) {
+        char* end = strchr(*input + 2, '\n');
+        if (end) {
+            *end = '\0';
+            char* delimiter = strdup(*input + 2);
+            *input = end + 1; // Skip to numbers
+            return delimiter;
+        }
+    }
+    return strdup(",");
+}
+
+static int sumNumbers(char* str, const char* delimiter) {
+    int sum = 0;
+    char* token = strtok(str, delimiter);
+    while (token != NULL) {
+        int num = parseNumber(token);
+        if (!shouldIgnore(num)) {
+            sum += num;
+        }
+        token = strtok(NULL, delimiter);
+    }
+    return sum;
+}
+
 static int add(const char* input) {
     if (input == NULL || strlen(input) == 0) {
         return 0;
     }
 
     char* str = strdup(input);
-    char* delimiter = ",";
-    int sum = 0;
-    int num;
-
-    // Check for custom delimiter
-    if (strncmp(str, "//", 2) == 0) {
-        char* end = strchr(str + 2, '\n');
-        if (end) {
-            *end = '\0';
-            delimiter = str + 2;  // Custom delimiter
-            str = end + 1;         // Skip to numbers
-        }
-    }
-
-    // Tokenize the input string
-    char* token = strtok(str, delimiter);
-    while (token != NULL) {
-        num = atoi(token);
-        if (num > 1000) {
-            token = strtok(NULL, delimiter);
-            continue; // Ignore numbers greater than 1000
-        }
-        sum += num;
-        token = strtok(NULL, delimiter);
-    }
-
+    char* delimiter = getDelimiter(&str);
+    
+    int result = sumNumbers(str, delimiter);
+    free(delimiter);
     free(str);
-    return sum;
+    
+    return result;
 }
 
 #endif // STRING_CALCULATOR_H
